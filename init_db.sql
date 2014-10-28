99e2a2a96dd051043d392b26e8659ea492522a46
Scott Tsao <caoshijie0501@gmail.com>
2014-10-28 20:23:23 +0800

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -7,3 +7,8 @@
  2. 用户打开App主页将自动显示最新获取到的25条新闻和公告（创建所有Department中的最新
     25个HNObject对象的索引）。如果阅读完毕，则不需要支持继续查询，故可以提前创建索引。
  */
+ 
+ db version1 on 2014/10/28
+ 每个学院均有自己的table。table的attribute包括：date，title，jsonfilepath，tag（type），link。
+ SQL的所有操作均在调用Mysql.rb。因此example使用branch with-a-easy-db，更改爬虫代码的时候只需更改table名字。
+ /*我现在觉得迭代一过后确实是要加两个tag的table的，确实想想数据传输会简单*/
