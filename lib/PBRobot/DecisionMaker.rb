d0466c6e77947ad538fae6992558209e39b596c9
ql94 <ql941016@gmail.com>
2015-04-01 15:12:11 +0800

    Update DecisionMaker.rb

--- lib/PBRobot/DecisionMaker.rb
+++ lib/PBRobot/DecisionMaker.rb
@@ -7,7 +7,9 @@ module PBRobot
     end
 
     def should_stop_at(url)
-      @config["link"] != nil && url == @config["link"]
+      @config["link"][0] != nil && url == @config["link"][0] && 
+      @config["link"][1] != nil && url == @config["link"][1] &&
+      @config["link"][2] != nil && url == @config["link"][2]
     end
 
   end
